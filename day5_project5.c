e2ce6d4fe1d96978dbc658aef33c00da45f7b232
alaatech-ai <aobeid098@gmail.com>
2026-02-28 23:52:54 +0200

    Create day5_project5.c

---
+++ day5_project5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){ 
+int number[3][4]={
+{1,2,3,4},
+{0,12,11,65},
+{20,21,100,3},
+};
+printf("%d\n", number[2][1]);
+printf("%d\n", number[0][3]);
+return 0;
+}

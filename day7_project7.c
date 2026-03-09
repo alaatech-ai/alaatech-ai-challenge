23fb8ae43ed365712ea44a4b8ac17cbdaf40ca50
alaatech-ai <aobeid098@gmail.com>
2026-03-09 17:34:39 +0200

    Create day7_project7.c

---
+++ day7_project7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+char board [3][3]={
+{'1','2','3'},
+{'4','5','6'},
+{'7','8','9'}
+};
+int i,j;
+for(i=0; i<3; i++){
+	for(j=0; j<3; j++){
+		printf(" %c ", board[i][j]);
+		if(j<2){
+			printf("|");          
+		}
+	}
+	printf("\n");
+	if(i<2){
+		printf("-----------\n");
+	}
+}
+	
+	
+		return 0;
+}

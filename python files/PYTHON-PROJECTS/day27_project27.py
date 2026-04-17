b33014b7a9c6205b403d8c912a69ae93fc7cd665
alaatech-ai <aobeid098@gmail.com>
2026-04-17 20:43:28 +0300

    Create day27_project27.py

---
+++ python files/PYTHON-PROJECTS/day27_project27.py
@@ -0,0 +1,14 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+    result = ""
+    for i in range(1, n + 1):
+        result += str(i)
+        if i < n:
+            result += " "
+
+    return result.strip()
+print(number_pattern(4))
+print(number_pattern(0))
